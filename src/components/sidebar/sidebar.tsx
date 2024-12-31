3bf2dd6fc5af8c8a86b293b496d0f84ea1f89c68
Hüsam <husamahmud@gmail.com>
2025-01-01 01:24:24 +0300

    style: update sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,9 +3,8 @@ import UserMenu from '@/components/sidebar/user-menu'
 
 const Sidebar = () => {
   return (
-    <aside className="flex flex-col items-center justify-between px-1 py-5">
+    <aside className="flex flex-col items-center justify-between py-5 pl-2 pr-1">
       <Logo />
-
       <UserMenu />
     </aside>
   )
