bfba8f1f567997edca569e8f09579b52bdd6837b
Hüsam <husamahmud@gmail.com>
2025-01-01 20:20:41 +0300

    enhancement: add google login loader

--- src/components/auth/google-signIn-button.tsx
+++ src/components/auth/google-signIn-button.tsx
@@ -1,17 +1,24 @@
 'use client'
 
+import { useState } from 'react'
 import { IoLogoGoogle } from 'react-icons/io5'
+import { TbLoader2 } from 'react-icons/tb'
 
 import { oauthLogin } from '@/db/supabase/services/auth.service'
 
 import { Button } from '@/components/ui/button'
 
 const GoogleSigninButton = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
   const handleGoogleLogin = async () => {
+    setIsLoading(true)
+
     const { url, error } = await oauthLogin('google')
 
     if (error) {
       console.error('Signin error:', error)
+      setIsLoading(false)
       return
     }
 
