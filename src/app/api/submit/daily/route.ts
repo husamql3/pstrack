2e9d385a5d4807c67ed71b2b7d4e47433f89ba32
Hüsam <husamahmud@gmail.com>
2025-01-18 22:25:09 +0300

    fix: update type

--- src/app/api/submit/daily/route.ts
+++ src/app/api/submit/daily/route.ts
@@ -1,4 +1,4 @@
-import { submissions } from '@prisma/client'
+import { type submissions } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { addCheckSubmission } from '@/prisma/dao/submissions.dao'
