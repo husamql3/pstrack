ef10f1000631260659c940686443bfa33b6100a3
husamahmud <husamahmud@gmail.com>
2025-01-27 16:42:25 +0300

    fix: rm roadmap till using neetcode roadmpa

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,11 +1,13 @@
-import { getRoadmap } from '@/hooks/get-roadmap'
+// import { getRoadmap } from '@/hooks/get-roadmap'
 
-import RoadmapView from '@/components/views/roadmap-view'
+// import RoadmapView from '@/components/views/roadmap-view'
+
+import { notFound } from 'next/navigation'
 
 const RoadmapPage = async () => {
-  const roadmap = await getRoadmap()
-
-  return <RoadmapView roadmap={roadmap} />
+  // const roadmap = await getRoadmap()
+  return notFound()
+  // return <RoadmapView roadmap={roadmap} />
 }
 
 export default RoadmapPage
