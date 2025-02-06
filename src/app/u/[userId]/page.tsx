57595b26f9048c06d99f5801351ca212a59ab593
husamahmud <husamahmud@gmail.com>
2025-02-06 17:21:39 +0300

    prettier: fix

--- src/app/u/[userId]/page.tsx
+++ src/app/u/[userId]/page.tsx
@@ -1,15 +1,10 @@
 import { fetchLeetcoder } from '@/prisma/dao/leetcoders.dao'
 
 import ProfileView from '@/components/views/profile-view'
-import { getUser } from '@/hooks/get-user'
-import { notFound } from 'next/navigation'
 
 const Page = async ({ params }: { params: Promise<{ userId: string }> }) => {
   const userId = (await params).userId
   const user = await fetchLeetcoder(userId)
-
-  const currUser = await getUser()
-  if (user?.id !== currUser?.id) notFound()
 
   return <ProfileView user={user!} />
 }
