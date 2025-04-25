500f5f80efa26a16854b3312d2eb3784e26d9f2e
husamahmud <husamahmud@gmail.com>
2025-04-25 23:53:13 +0300

    responsive login btn (closes #101)

--- src/app/_components/user-auth.tsx
+++ src/app/_components/user-auth.tsx
@@ -1,4 +1,5 @@
 import type { User } from '@supabase/supabase-js'
+import { LucideLogIn } from 'lucide-react'
 
 import { UserMenu } from '@/app/(auth)/_components/user-menu'
 import { StaticLink } from '@/ui/hover-link'
@@ -6,7 +7,17 @@ import { StaticLink } from '@/ui/hover-link'
 export const UserAuth = async ({ user }: { user: User | null }) => {
   return (
     <div className="flex items-center">
-      {user ? <UserMenu user={user} /> : <StaticLink href="/login">Login</StaticLink>}
+      {user ? (
+        <UserMenu user={user} />
+      ) : (
+        <StaticLink
+          href="/login"
+          className="px-5"
+        >
+          <span className="hidden sm:block">Login</span>
+          <LucideLogIn className="block size-5 sm:hidden" />
+        </StaticLink>
+      )}
     </div>
   )
 }
