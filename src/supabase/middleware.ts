5e5cb6c2e2af8aeefc3436fa1f86d1f0dba99d99
husamahmud <husamahmud@gmail.com>
2025-05-03 15:08:33 +0300

    update v

--- src/supabase/middleware.ts
+++ src/supabase/middleware.ts
@@ -24,7 +24,7 @@ export async function updateSession(request: NextRequest) {
   if (pathname.startsWith('/resources') && !user) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
-  
+
   if (user && pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url))
   }
