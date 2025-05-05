c66955a6b58bf067fc6ee60a0742df1844f4b06e
Hüsam <husamahmud@gmail.com>
2025-05-05 15:47:52 +0300

    add ratelimit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## [2.1.14]
+add rate limit for emails
+
 ## [2.1.13]
 update roadmap
 
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,18 +1,46 @@
 import pLimit from 'p-limit'
 
-export const VERSION = '2.1.13'
-export const VISIBLE_COUNT = 20
-export const PROBLEM_BASE_URL = 'https://leetcode.com/problems'
-export const LEETCODE_GQL_BASE_URL = 'https://leetcode.com/graphql'
-export const AUTHOR_EMAIL = 'husamahmud@gmail.com'
-export const ADMINS_EMAILS = ['husamahmud@gmail.com', 'nezhataghy@gmail.com']
-export const PROTECTED_ROUTES = ['/dashboard', '/profile']
-export const UNSOLVED_THRESHOLD = 6
+/**
+ * App version
+ */
+export const VERSION = '2.1.14'
+
+/**
+ * Repository information
+ */
 export const USERNAME = 'husamahmud'
 export const REPO_NAME = 'pstrack'
-export const MAX_LEETCODERS = 35
+
+/**
+ * UI configuration
+ */
+export const VISIBLE_COUNT = 20
+
+/**
+ * LeetCode URLs
+ */
+export const PROBLEM_BASE_URL = 'https://leetcode.com/problems'
+export const LEETCODE_GQL_BASE_URL = 'https://leetcode.com/graphql'
+
+/**
+ * Admin management
+ */
+export const AUTHOR_EMAIL = 'husamahmud@gmail.com'
+export const ADMINS_EMAILS = ['husamahmud@gmail.com', 'nezhataghy@gmail.com']
 export const NOT_STARTED_GROUPS = [7]
 
+/**
+ * Authentication and authorization
+ */
+export const PROTECTED_ROUTES = ['/dashboard', '/profile']
+
+/**
+ * Problem tracking thresholds
+ */
+export const UNSOLVED_THRESHOLD = 6
+export const MAX_LEETCODERS = 35
+
+// API request configuration
 export const BATCH_SIZE = 20
 export const DELAY_MS = 1000
 export const LIMIT = pLimit(5)
