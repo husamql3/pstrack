1f26d391ae6e8cfb80dd162083053dada2dd0489
Hüsam <husamahmud@gmail.com>
2025-06-01 23:02:35 +0300

    sort folders by names

--- src/app/(public)/resources/_components/resources-tree.tsx
+++ src/app/(public)/resources/_components/resources-tree.tsx
@@ -5,31 +5,35 @@ import { Files, File, Folder } from '@/ui/files'
 export const ResourcesTree = ({ resources }: { resources: GroupedResources }) => {
   return (
     <Files className="w-full border-none bg-transparent">
-      {resources.map((resource) => (
-        <Folder
-          key={resource.topic}
-          name={resource.topic}
-          className="cursor-pointer"
-        >
-          {resource.folders.map((folder) => (
-            <Folder
-              key={folder.type}
-              name={folder.type}
-              className="cursor-pointer"
-            >
-              {folder.resources.map((item) => (
-                <File
-                  key={item.id}
-                  name={item.title}
-                  href={item.url}
-                  type={item.type}
-                  contributor={item.contributor}
-                />
+      {resources
+        .sort((a, b) => a.topic.localeCompare(b.topic))
+        .map((resource) => (
+          <Folder
+            key={resource.topic}
+            name={resource.topic}
+            className="cursor-pointer"
+          >
+            {resource.folders
+              .sort((a, b) => a.type.localeCompare(b.type))
+              .map((folder) => (
+                <Folder
+                  key={folder.type}
+                  name={folder.type}
+                  className="cursor-pointer"
+                >
+                  {folder.resources.map((item) => (
+                    <File
+                      key={item.id}
+                      name={item.title}
+                      href={item.url}
+                      type={item.type}
+                      contributor={item.contributor}
+                    />
+                  ))}
+                </Folder>
               ))}
-            </Folder>
-          ))}
-        </Folder>
-      ))}
+          </Folder>
+        ))}
     </Files>
   )
 }
