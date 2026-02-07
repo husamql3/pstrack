33920181972bb4969691a050e5822bcb40fd6ecf
Hüsam <husamahmud@gmail.com>
2026-02-08 02:13:33 +0300

    fix deploy

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,3 @@
-import { serve } from "@hono/node-server";
-
 import { env } from "@/env";
 import { success } from "@/lib/response";
 import { authRouter } from "@/routes/auth.route";
@@ -18,29 +16,32 @@ const app = createApp()
 	})
 	.route("/api/auth", authRouter);
 
-const server = serve(
-	{
-		fetch: app.fetch,
-		port: env.PORT,
-	},
-	() => {
-		console.log(`Server is running on http://localhost:${env.PORT} in ${env.NODE_ENV} mode`);
-	},
-);
+if (env.NODE_ENV === "development") {
+	const { serve } = await import("@hono/node-server");
+	const server = serve(
+		{
+			fetch: app.fetch,
+			port: env.PORT,
+		},
+		() => {
+			console.log(`Server is running on http://localhost:${env.PORT} in ${env.NODE_ENV} mode`);
+		},
+	);
 
-process.on("SIGINT", () => {
-	server.close();
-	process.exit(0);
-});
-
-process.on("SIGTERM", () => {
-	server.close((err) => {
-		if (err) {
-			console.error(err);
-			process.exit(1);
-		}
+	process.on("SIGINT", () => {
+		server.close();
 		process.exit(0);
 	});
-});
+
+	process.on("SIGTERM", () => {
+		server.close((err) => {
+			if (err) {
+				console.error(err);
+				process.exit(1);
+			}
+			process.exit(0);
+		});
+	});
+}
 
 export default app;
